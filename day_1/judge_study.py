e5dc3c43cf6cad920e77ec93bce55e304ab70ffb
441238223@qq.com <Githubpwd123>
2020-11-04 18:37:10 +0800

    判断和循环的学习

---
+++ day_1/judge_study.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-04 5:45 PM
+result = 80
+if (result > 60 and result <80):
+    print("及格")
+elif (result >= 80 and result < 90):
+    print("better")
+elif (result >= 90):
+    print("best")
+else:
+    print("rubbish")
+
+
+num = 6
+#num = ''   # so bad
+if num:
+    print("You are right")
+else:
+    print("so bad")
---
+++ day_1/loop_study.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-04 6:28 PM
+for letter in "hello 萧然":
+    print(letter)
+
+for i in range(6):
+    print(i)
