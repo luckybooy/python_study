d608e97e098d2757c79197051208b83a9cc6ba34
441238223@qq.com <Githubpwd123>
2020-11-04 13:55:57 +0800

    list学习

---
+++ day_1/list_study.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-04 11:50 AM
+name = ['小明', '李华', '钱庆', '田珠']
+print(name)
+print("--------------------------------")
+print(name[2])
+print("--------------------------------")
+name.append('马总')
+print(name)
