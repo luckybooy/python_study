f44f3644b02f3a4d71c2279e34a27787c6c44d62
441238223@qq.com <Githubpwd123>
2020-11-05 16:16:25 +0800

    自定义函数练习

--- day_2/exercise_1.py
+++ day_2/exercise_1.py
@@ -14,5 +14,15 @@ print("*********************separator********************")
 
 for i in range(1, 10):
     for j in range(1, i+1):
-        print("{}x{}={}\t".format(i, j, i*j), end='')
+        print("{}x{}={}\t".format(j, i, i*j), end='')
     print()
+
+
+
+#判断是否是闰年
+YEAR = int(input("请入一个年份："))
+print(type(YEAR))
+if(YEAR%400 ==0 or (YEAR %4==0 and YEAR%100 != 0)):
+    print("{0}年是闰年".format(YEAR))
+else:
+    print("{0}年不是闰年".format(YEAR))
---
+++ day_2/function_study.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-05 4:07 PM
+# 定义一个函数
+"""
+格式：
+def 函数名(参数列表):  ":" 冒号很重要 表示函数体的开始
+    函数体
+    return 返回值(没有的话默认返回None)
+"""
+def sum (num1, num2):
+    res = num1 + num2
+    return res
+
+print(sum(1, 3))
