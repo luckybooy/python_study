ec5db310dd2a7205e87257b827e410a9be01925b
xiaoran <441238223@qq.com>
2020-11-17 16:50:05 +0800

    判断是不是主函数

---
+++ day_7/is_main_stu.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-17 4:46 PM
+if __name__ == '__main__':
+    print("main")
+else:
+    print("not main")
---
+++ day_7/is_not_main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-17 4:47 PM
+
+import is_main_stu
+#直接导入is_main_stu 模块 则输出是 not main
