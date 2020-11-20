1e62313e5bba99fef9dcfeaf07bc1e4abc4dbe80
xiaoran <441238223@qq.com>
2020-11-20 10:09:15 +0800

    动态类学习

---
+++ day_10/dynamic_class_stu.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-20 9:35 AM
+class Hello(object):
+    def hello(self, name='py'):
+        print('Hello,', name)
+
+if __name__ == '__main__':
+    print(type(Hello))
---
+++ day_10/dynamic_hello_test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-20 9:38 AM
+from day_10.dynamic_class_stu import  Hello
+h = Hello()
+h.hello()
+print(type(Hello))
+print(type(h))
