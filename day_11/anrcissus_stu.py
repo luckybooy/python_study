563fc7fcb98e80d91c73ee04bd216633abc1d0b4
xiaoran <441238223@qq.com>
2020-11-24 11:45:14 +0800

    正整数的反转，如：将123456 转换为654321

--- day_11/anrcissus_stu.py
+++ day_11/anrcissus_stu.py
@@ -5,10 +5,22 @@
 
 # 计算100～1000以内，各个数位上的数字的三次方之和 的数字有哪些
 # 找水仙花【narcissus】数
-
+"""
 for num in range(100, 1000):
     low = num % 10
     mid = num // 10 % 10
     high = num // 100
     if num == low**3 + mid**3 + high**3:
         print(num)
+"""
+
+"""
+正整数的反转
+"""
+
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
