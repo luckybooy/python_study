93ff72b7b9b750d5e3c884b6b784c555b7c3d7fa
xiaoran <441238223@qq.com>
2020-11-24 11:12:41 +0800

    找水仙花数 水仙花英文【narcissus】

---
+++ day_11/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-24 10:58 AM
---
+++ day_11/anrcissus_stu.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# project_name: python_study
+# author:xiaoran
+# date:2020-11-24 10:59 AM
+
+# 计算100～1000以内，各个数位上的数字的三次方之和 的数字有哪些
+# 找水仙花【narcissus】数
+
+for num in range(100, 1000):
+    low = num % 10
+    mid = num // 10 % 10
+    high = num // 100
+    if num == low**3 + mid**3 + high**3:
+        print(num)
