678bbda0c24e5c254bf2435c2121c84cbd2893e0
xiaoran <441238223@qq.com>
2020-12-15 14:08:19 +0800

    实现动画效果

--- python_ui/game_one.py
+++ python_ui/game_one.py
@@ -4,7 +4,7 @@
 # date:2020-12-15 10:43 AM
 import pygame
 
-
+"""
 def main():
     # 初始化导入的pygame中的模块
     pygame.init()
@@ -18,11 +18,9 @@ def main():
     ball_image = pygame.image.load('./src/zuqiu.png')
     # 在窗口上渲染图像
     screen.blit(ball_image, (50, 50))
-    """
-    自己画的方式来展示图像
+    #自己画的方式来展示图像
     # # 绘制一个圆(参数：屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
-    # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
-    """
+    # # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
     # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
     pygame.display.flip()
     running = True
@@ -32,6 +30,29 @@ def main():
         for even in pygame.event.get():
             if even.type == pygame.QUIT:
                 running = False
+"""
+
+def main():
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口的尺寸
+    screen = pygame.display.set_mode((800, 600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('打球吃小球')
+    # 定义变量来表示小球在屏幕上的位置
+    x, y = 50, 50
+    running = True
+    while running:
+        # 从消息队列中获取事件并对事件进行处理
+        for even in pygame.event.get():
+            if even.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0,), (x, y), 30, 0)
+        pygame.display.flip()
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 5, y + 5
 
 
 if __name__ == '__main__':
