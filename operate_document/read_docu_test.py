5662d2f442d12f52d1ada0457b13bec0a90d85da
xiaoran <441238223@qq.com>
2020-12-18 17:27:44 +0800

    读取文件添加异常处理

--- operate_document/read_docu_test.py
+++ operate_document/read_docu_test.py
@@ -3,9 +3,19 @@
 # author:xiaoran
 # date:2020-12-18 5:20 PM
 def main():
-    f = open('__init__.py', 'r', encoding='utf-8')
-    print(f.read())
-    f.close()
+    f = None
+    try:
+        f = open('__init__.py', 'r', encoding='utf-8')
+        print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件夹')
+    except LookupError:
+        print('指定了未知的编码')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误')
+    finally:
+        if f:
+            f.close()
 
 
 if __name__ == '__main__':
