45352bf3d142f7184efc8691a729e3f83cf42571
xiaoran <441238223@qq.com>
2021-03-18 23:23:32 +0800

    数组列表学习增删改

--- day_4/shuju_ste.py
+++ day_4/shuju_ste.py
@@ -10,3 +10,26 @@ print(names[1:3])
 print(names[1])
 # -1 选择最后一个元素
 print(names[-1])
+# 取最后两个
+print(names[-2:])
+# 取下标为2及以后的元素
+print(names[2:])
+# 追加
+names.append('王强')
+print(names)
+
+# 在下标为1的位置插入
+names.insert(1, "秦叔宝")
+print(names)
+
+# 替换 脚标位置的元素
+names[2] = 'lisi'
+print(names)
+
+names.pop(2)
+print(names)
+
+# 根据元素定位 在数组中的位置
+index = names.index('钱七')
+print(index)
+
