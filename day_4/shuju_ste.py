ece943cab9136594c1de16cee32f119636c950ac
xiaoran <441238223@qq.com>
2021-03-18 23:13:01 +0800

    数组列表学习

---
+++ day_4/shuju_ste.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# FileName : shuju_ste.py 
+# Author : xiaoran
+# Date : 2021-03-18 23:10
+names = ['张三', '李四', '王武', '马六', '钱七']
+print(names)
+
+print(names[1:3])
+
+print(names[1])
+# -1 选择最后一个元素
+print(names[-1])
