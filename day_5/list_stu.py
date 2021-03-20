5651b449469133e0eacb3e660fbcdfcce6e32d93
xiaoran <441238223@qq.com>
2021-03-20 23:55:21 +0800

    enumrate 遍历数组，这个方法可以带索引

--- day_5/list_stu.py
+++ day_5/list_stu.py
@@ -9,3 +9,17 @@ product_list = [
     ("平板", 8000)
 ]
 print(product_list[0])
+
+salary = input("请输入您的工资：")
+# isdigit() 方法用来判断 值的是不是数字
+if salary.isdigit():
+    salary = int(salary)
+
+    # 遍历数组中的元素
+    for item in product_list:
+        print(item)
+
+    print("__________________________________________")
+    # 2.遍历数组带索引
+    for index, item in enumerate(product_list):
+        print(index,item)
