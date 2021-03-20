21a3b136bf26dbf1854a31ebb92b30bf3eaa277e
xiaoran <441238223@qq.com>
2021-03-20 23:32:28 +0800

    list 学习

--- day_5/list_stu.py
+++ day_5/list_stu.py
@@ -2,3 +2,10 @@
 # FileName : list_stu.py 
 # Author : xiaoran
 # Date : 2021-03-20 23:29
+product_list = [
+    ("手机", 5000),
+    ("手表", 2500),
+    ("电脑", 12000),
+    ("平板", 8000)
+]
+print(product_list[0])
