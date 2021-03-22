74ea1dc92f212f9bb650460ee76758d6b17864b4
xiaoran <441238223@qq.com>
2021-03-22 21:53:06 +0800

    info.get(key) 推荐的字典查找方法

--- day_4/dict_stu.py
+++ day_4/dict_stu.py
@@ -16,7 +16,12 @@ info[1001] = "zhangsan"
 print(info)
 # 查
 print(info[1003])
+print("===================第二种查找方法===================")
+# 第二种查找的方式--推荐(咋没查到时不会报错)
+print(info.get(1001))
 
+info['10111'] = "狗蛋儿"
+print(info.get('10111'))
 # 删除1001对应的元素
 info.pop(1001)
 print(info) # 推荐使用 的删除方法
