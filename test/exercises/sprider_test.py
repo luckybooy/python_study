b761fb24d75992105621fd2150ca8460d5123e10
xiaoran <441238223@qq.com>
2021-07-27 10:23:08 +0800

    push test

---
+++ test/exercises/sprider_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# FileName : sprider_test.py 
+# Author : xiaoran
+# Date : 2021-07-27 10:19
+import requests
+
+r = requests.get('https://www.qiushibaike.com/text/')  # 打开网址，一般我们会设置 请求头，来更逼真的模拟浏览器，下文有介绍
+print(r.text)
